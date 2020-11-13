2445417ccfd47d4a19626e8a6da0a8ccdc2ca686
jesus <jdcv2823@gmail.com>
2020-11-13 14:10:17 -0400

    Primer commit

---
+++ aprendiendo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+   string pasar_el_año = " ";
+   
+   cout<<"algun dia pasaras el año?: ";
+   cin>>pasar_el_año;
+
+   if(pasar_el_año == "si lo pasare"){
+   	cout<<"SI LO VOY A PASAR :D";
+   }
+   else{
+        cout<<"No lo pasare :c";
+   }
+
+   return 0;  
+}
